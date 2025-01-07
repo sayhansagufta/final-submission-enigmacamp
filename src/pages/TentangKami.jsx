381a96b561aff87ab73dc436669213ffff70d78b
Andreas Kurniawan Rusli <andreaskr0120@gmail.com>
2025-01-07 21:49:43 +0700

    feat: Menambahkan animasi di setiap bagiannya

--- src/pages/TentangKami.jsx
+++ src/pages/TentangKami.jsx
@@ -78,39 +78,39 @@ function TentangKami() {
     <div>
       {/* header */}
         <div className="bg-slate-100 h-[400px] flex flex-col justify-center align-middle">
-            <h1 className="text-center font-bold text-6xl">Tentang <span className="text-indigo-700" >Kami</span></h1>
-            <p className="text-center text-2xl p-14">Platform digital untuk mencatat, berbagi, dan menginspirasi. Temukan kemudahan mencatat momen dan ide terbaikmu bersama kami.</p>
+            <h1 className="text-center font-bold text-6xl" data-aos="fade-down" data-aos-duration="1000">Tentang <span className="text-indigo-700" >Kami</span></h1>
+            <p className="text-center text-2xl p-14" data-aos="fade-up" data-aos-duration="2500">Platform digital untuk mencatat, berbagi, dan menginspirasi. Temukan kemudahan mencatat momen dan ide terbaikmu bersama kami.</p>
         </div>
 
         {/* body */}
         <div className="p-16 bg-white">
             {/* Tentang aplikasi */}
             <div className="h-[400px] flex flex-col flex-wrap align-middle justify-center">
-                <h1 className="text-4xl font-bold">Tentang <span className="text-indigo-700" >Aplikasi</span></h1>
+                <h1 className="text-4xl font-bold" data-aos="fade-right" data-aos-duration="1000">Tentang <span className="text-indigo-700" >Aplikasi</span></h1>
                 <br />
-                <div className="w-[600px]">
+                <div className="w-[600px]" data-aos="fade-right" data-aos-duration="1000">
                     <p className="text-2xl">Catatan Kita adalah ruang digital yang membantu individu dan tim mencatat, mengelola ide, dan berbagi informasi dengan mudah. Kami percaya bahwa setiap catatan memiliki nilai yang dapat menginspirasi.</p>
                 </div>
-                <img src={about1} alt="about1" className="m-3"/>
+                <img src={about1} alt="about1" className="m-3" data-aos="fade-left" data-aos-duration="1000"/>
             </div>
 
             {/* Fitur Utama */}
             <div className="mt-24">
-                <h1 className="text-4xl font-bold text-center">Fitur <span className="text-indigo-700">Utama</span></h1>
+                <h1 className="text-4xl font-bold text-center" data-aos="zoom-in" data-aos-duration="1000">Fitur <span className="text-indigo-700">Utama</span></h1>
                 <div className="flex flex-row justify-between pt-14">
-                    <div className="w-[200px]">
+                    <div className="w-[200px]" data-aos="flip-left" data-aos-duration="1000">
                         <center><img src={feature1} alt="feature1"/></center>
                         <p className="text-2xl font-semibold text-center">Pencatatan Digital</p>
                     </div>
-                    <div className="w-[200px]">
+                    <div className="w-[200px]" data-aos="flip-left" data-aos-duration="1000">
                         <center><img src={feature2} alt="feature2"/></center>
                         <p className="text-2xl font-semibold text-center">Pengelolaan yang rapih</p>
                     </div>
-                    <div className="w-[200px]">
+                    <div className="w-[200px]" data-aos="flip-left" data-aos-duration="1000">
                         <center><img src={feature3} alt="feature3"/></center>
                         <p className="text-2xl font-semibold text-center">Kolaborasi secara real time</p>
                     </div>
-                    <div className="w-[200px]">
+                    <div className="w-[200px]" data-aos="flip-left" data-aos-duration="1000">
                         <center><img src={feature4} alt="feature4"/></center>
                         <p className="text-2xl font-semibold text-center">Personalisasi</p>
                     </div>
@@ -118,8 +118,8 @@ function TentangKami() {
             </div>
 
             {/* developer */}
-            <div className="mt-24">
-                <h1 className="text-4xl font-bold text-center">Developer</h1>
+            <div className="mt-24" >
+                <h1 className="text-4xl font-bold text-center" data-aos="fade-up" data-aos-duration="1000">Developer</h1>
 
                 <div className="sliderAx h-auto">
                   <div id="slider-1" className="container mx-auto flex flex-row justify-around pt-14 gap-5">
@@ -145,39 +145,12 @@ function TentangKami() {
                         <img src={dev5} alt="dev5" className="w-full object-cover object-top h-[250px]"/>
                         <p className="text-2xl text-center font-semibold mt-3">Shayhan Sagufta</p>
                     </div>
-                    <div>
-                        <img src={dev1} alt="dev1" className="w-full object-cover object-top h-[250px]" />
-                        <p className="text-2xl text-center font-semibold mt-3">Luthfi Hadhit Habibi</p>
-                    </div>
                   </div>
                 </div>
                 <div className="flex justify-between w-14 mx-auto pb-2">
                   <button id="sButton1" onClick={sliderButton1} className="bg-purple-400 rounded-full w-6 pb-2"></button>
                   <button id="sButton2" onClick={sliderButton2} className="bg-purple-400 rounded-full w-6 pb-2"></button>
                 </div>
-
-                {/* <div className="flex flex-row justify-between pt-14 gap-5">
-                    <div>
-                        <img src={dev1} alt="dev1" className="w-full object-cover object-top h-[250px]" />
-                        <p className="text-2xl font-semibold mt-3">Luthfi Hadhit Habibi</p>
-                    </div>
-                    <div>
-                        <img src={dev2} alt="dev2" className="w-full object-cover object-top h-[250px]"/>
-                        <p className="text-2xl font-semibold mt-3">Muhamad Syaiful Bahri</p>
-                    </div>
-                    <div className="w-[250px]">
-                        <img src={dev3} alt="dev3" className="w-full object-cover object-top h-[250px]"/>
-                        <p className="text-2xl font-semibold mt-3">Andreas Kurniawan Rusli</p>
-                    </div>
-                    <div className="w-[250px]">
-                        <img src={dev4} alt="dev4" className="w-full object-cover object-top h-[250px]"/>
-                        <p className="text-2xl font-semibold mt-3">Elman Nabil</p>
-                    </div>
-                    <div className="w-[250px]">
-                        <img src={dev5} alt="dev5" className="w-full object-cover object-top h-[250px]"/>
-                        <p className="text-2xl font-semibold mt-3">Shayhan Sagufta</p>
-                    </div>
-                </div> */}
             </div>
         </div>
     </div>
