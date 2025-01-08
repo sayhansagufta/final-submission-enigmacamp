bac9a06b618c789c7d95955231604a8a2d7172c4
Andreas Kurniawan Rusli <andreaskr0120@gmail.com>
2025-01-08 17:02:57 +0700

    fix: perbaikan bagian developer dengan menggunakan flowbite

--- src/pages/TentangKami.jsx
+++ src/pages/TentangKami.jsx
@@ -9,71 +9,10 @@ import dev2 from "../assets/about/Syaiful.jpg"
 import dev3 from "../assets/about/Andre.png"
 import dev4 from "../assets/about/Nabil.jpg"
 import dev5 from "../assets/about/Shayhan.jpg"
+import { Carousel } from "flowbite-react";
 
 
 function TentangKami() {
-  var cont=0;
-
-  const loopSlider = () => {
-    var xx= setInterval(function(){
-      switch(cont)
-      {
-      case 0:
-        {
-          $("#slider-1").fadeOut(400);
-          $("#slider-2").delay(400).fadeIn(400);
-          $("#sButton1").removeClass("bg-purple-800");
-          $("#sButton2").addClass("bg-purple-800");
-          cont=1;
-          
-          break;
-        }
-      case 1:
-        {
-        
-          $("#slider-2").fadeOut(400);
-          $("#slider-1").delay(400).fadeIn(400);
-          $("#sButton2").removeClass("bg-purple-800");
-          $("#sButton1").addClass("bg-purple-800");
-          
-          cont=0;
-          
-          break;
-        }
-      }
-    },8000);
-  }
-
-  const reinitLoop = (time) => {
-    clearInterval(xx);
-    setTimeout(loopSlider(),time);
-  }
-
-  const sliderButton1 = () => {
-    $("#slider-2").fadeOut(400);
-    $("#slider-1").delay(400).fadeIn(400);
-    $("#sButton2").removeClass("bg-purple-800");
-    $("#sButton1").addClass("bg-purple-800");
-    reinitLoop(4000);
-    cont=0
-  }
-
-  const sliderButton2 = () => {
-    $("#slider-1").fadeOut(400);
-    $("#slider-2").delay(400).fadeIn(400);
-    $("#sButton1").removeClass("bg-purple-800");
-    $("#sButton2").addClass("bg-purple-800");
-    reinitLoop(4000);
-    cont=1
-  }
-
-  $(window).ready(() => {
-    $("#slider-2").hide();
-    $("#sButton1").addClass("bg-purple-800");
-    
-    loopSlider();
-  })
-
   return (
     <div>
       {/* header */}
@@ -116,43 +55,39 @@ function TentangKami() {
                     </div>
                 </div>
             </div>
-
-            {/* developer */}
-            <div className="mt-24" >
-                <h1 className="text-4xl font-bold text-center" data-aos="fade-up" data-aos-duration="1000">Developer</h1>
-
-                <div className="sliderAx h-auto">
-                  <div id="slider-1" className="container mx-auto flex flex-row justify-around pt-14 gap-5">
+          </div>
+          {/* developer */}
+          <div className="pt-10 bg-blue-200 rounded-lg" data-aos="fade-up" data-aos-duration="1000">
+              <h1 className="text-4xl font-bold text-center">Developer</h1>
+              <div className="h-96">
+                <Carousel>
+                  <div className="mx-auto flex flex-row justify-around pt-14 pb-20">
                     <div>
-                        <img src={dev1} alt="dev1" className="w-full object-cover object-top h-[250px]" />
+                        <img src={dev1} alt="dev1" className="w-[250px] object-cover object-top h-[250px] rounded-full" />
                         <p className="text-2xl text-center font-semibold mt-3">Luthfi Hadhit Habibi</p>
                     </div>
                     <div>
-                        <img src={dev2} alt="dev2" className="w-full object-cover object-top h-[250px]"/>
+                        <img src={dev2} alt="dev2" className="w-[250px] object-cover object-top h-[250px] rounded-full"/>
                         <p className="text-2xl text-center font-semibold mt-3">Muhamad Syaiful Bahri</p>
                     </div>
                     <div className="w-[250px]">
-                        <img src={dev3} alt="dev3" className="w-full object-cover object-top h-[250px]"/>
+                        <img src={dev3} alt="dev3" className="w-[250px] object-cover object-top h-[250px] rounded-full"/>
                         <p className="text-2xl text-center font-semibold mt-3">Andreas Kurniawan Rusli</p>
                     </div>
                   </div>
-                  <div id="slider-2" className="container mx-auto flex flex-row justify-around pt-14 pb-10 gap-5"> 
+                  <div className="mx-auto flex flex-row justify-around pt-14 pb-20">
                     <div className="w-[250px]">
-                        <img src={dev4} alt="dev4" className="w-full object-cover object-top h-[250px]"/>
+                        <img src={dev4} alt="dev4" className="w-[250px] object-cover object-top h-[250px] rounded-full"/>
                         <p className="text-2xl text-center font-semibold mt-3">Elman Nabil</p>
                     </div>
                     <div className="w-[250px]">
-                        <img src={dev5} alt="dev5" className="w-full object-cover object-top h-[250px]"/>
+                        <img src={dev5} alt="dev5" className="w-[250px] object-cover object-top h-[250px] rounded-full"/>
                         <p className="text-2xl text-center font-semibold mt-3">Shayhan Sagufta</p>
                     </div>
                   </div>
-                </div>
-                <div className="flex justify-between w-14 mx-auto pb-2">
-                  <button id="sButton1" onClick={sliderButton1} className="bg-purple-400 rounded-full w-6 pb-2"></button>
-                  <button id="sButton2" onClick={sliderButton2} className="bg-purple-400 rounded-full w-6 pb-2"></button>
-                </div>
-            </div>
-        </div>
+                </Carousel>
+              </div>
+          </div>
     </div>
   );
 }
